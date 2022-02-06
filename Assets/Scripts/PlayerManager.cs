a49d4037786d7b2a0e9a47e356dfc9def1d8ff87
Oleh Yatskiv <92208702+Oleh325@users.noreply.github.com>
2022-02-06 14:33:39 +0200

    Kmh conflict fix (#10)
    * Added pause menu
    
    * Added proper scaling, background, loser menu and death mechanics
    
    * Addad pavement, ladder, background, more buildings and more

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private LayerMask buildingsLayer;
+    [SerializeField] private LayerMask jumpableLayer;
+    [SerializeField] private GameObject loserMenu;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private CameraController cmrctrl;
     private Rigidbody2D player;
     private BoxCollider2D collider2d;
+    private Animator anim;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         player = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<BoxCollider2D>();
     }
@@ -20,11 +25,17 @@ public class PlayerManager : MonoBehaviour
 
         if (horizontalInput > 0.01f)
         {
-            transform.localScale = new Vector3(0.07521388f, 0.139201f, 1);
+            transform.localScale = new Vector3(0.3348586f, 0.3348586f, 1);
+            anim.SetBool("isRunning", true);
         }
         else if (horizontalInput < -0.01f)
         {
-            transform.localScale = new Vector3(-0.07521388f, 0.139201f, 1);
+            transform.localScale = new Vector3(-0.3348586f, 0.3348586f, 1);
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
         }
     }
 
@@ -32,12 +43,23 @@ public class PlayerManager : MonoBehaviour
     {
         if (Input.GetKeyDown("up") && IsGrounded())
         {
-            player.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            player.AddForce(new Vector2(0, 9), ForceMode2D.Impulse);
         }
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(collider2d.bounds.center, collider2d.bounds.size, 0f, Vector2.down, 0.1f, buildingsLayer);
+        return Physics2D.BoxCast(collider2d.bounds.center, collider2d.bounds.size, 0f, Vector2.down, 0.1f, jumpableLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            transform.position = new Vector3(690f, -170f, -8);
+            canvas.SetActive(true);
+            loserMenu.SetActive(true);
+            cmrctrl.GetComponent<CameraController>().Pause();
+        }
     }
 }
