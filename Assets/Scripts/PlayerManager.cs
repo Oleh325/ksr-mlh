fcffa0799d79fff88823034fa471fcf6d6570a82
DESKTOP-POSS0RQ\olega <olegackiv956@gmail.com>
2022-02-06 07:03:09 +0200

    Added proper scaling, background, loser menu and death mechanics

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,11 +4,16 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private LayerMask buildingsLayer;
+    [SerializeField] private GameObject loserMenu;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private CameraController cmrctrl;
     private Rigidbody2D player;
     private BoxCollider2D collider2d;
+    private Animator anim;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         player = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<BoxCollider2D>();
     }
@@ -20,11 +25,17 @@ public class PlayerManager : MonoBehaviour
 
         if (horizontalInput > 0.01f)
         {
-            transform.localScale = new Vector3(0.07521388f, 0.139201f, 1);
+            transform.localScale = new Vector3(0.3348586f, 0.3348586f, 1);
+            anim.SetBool("isRunning", true);
         }
         else if (horizontalInput < -0.01f)
         {
-            transform.localScale = new Vector3(-0.07521388f, 0.139201f, 1);
+            transform.localScale = new Vector3(-0.3348586f, 0.3348586f, 1);
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
         }
     }
 
@@ -32,12 +43,23 @@ public class PlayerManager : MonoBehaviour
     {
         if (Input.GetKeyDown("up") && IsGrounded())
         {
-            player.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            player.AddForce(new Vector2(0, 9), ForceMode2D.Impulse);
         }
     }
 
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(collider2d.bounds.center, collider2d.bounds.size, 0f, Vector2.down, 0.1f, buildingsLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Death")
+        {
+            transform.position = new Vector3(690.5f, -170f, -8);
+            canvas.SetActive(true);
+            loserMenu.SetActive(true);
+            cmrctrl.GetComponent<CameraController>().Pause();
+        }
     }
 }
