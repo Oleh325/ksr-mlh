15f4880984820e52325878d12b8222531c2059bb
PHentosh <petro.hentosh@gmail.com>
2022-02-06 12:14:19 +0200

    Upload Coffe Rain and Umbrella

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -6,17 +8,40 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private LayerMask buildingsLayer;
     private Rigidbody2D player;
     private BoxCollider2D collider2d;
+    private int coffesec = 0;
+    private float jumpForce = 6.0f;
+    private int UnmbrelluNum = 0;
+    private float rainchance = 1.0f;
+    private ParticleSystem particle;
+    private bool check = false;
+
 
     private void Awake()
     {
         player = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<BoxCollider2D>();
+        StartCoroutine(coffeclock());
+        particle = GameObject.Find("Rain").GetComponent<ParticleSystem>();
     }
 
     private void FixedUpdate()
     {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
-        player.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, player.velocity.y);
+        if (particle.isPlaying && UnmbrelluNum<=0){
+            check = false;
+            rainchance = 0.5f;
+        } else if(particle.isPlaying && UnmbrelluNum>0){
+            check = true;
+        } else {
+            if (check){
+                UnmbrelluNum--;
+                check = false;
+            }
+            rainchance = 1;
+        }
+        player.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * rainchance, player.velocity.y);
+        
 
         if (horizontalInput > 0.01f)
         {
@@ -32,12 +57,45 @@ public class PlayerManager : MonoBehaviour
     {
         if (Input.GetKeyDown("up") && IsGrounded())
         {
-            player.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+            player.AddForce(new Vector2(0, jumpForce * rainchance), ForceMode2D.Impulse);
         }
     }
 
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(collider2d.bounds.center, collider2d.bounds.size, 0f, Vector2.down, 0.1f, buildingsLayer);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Umbrella")){
+                Destroy(collision.gameObject);
+                UnmbrelluNum++;
+                jumpForce = 3.5f;
+                player.gravityScale = 1.0f;
+
+        }
+
+        if (collision.gameObject.CompareTag("coffe")){
+                Destroy(collision.gameObject);
+                speed = speed * 1.5f;
+                StartCoroutine(coffeclock());
+                coffesec = 0;
+                
+        }
+    }
+
+    IEnumerator coffeclock(){
+        while(true){
+            coffesec++;
+            if (coffesec == 20){
+                coffesec = 0;
+                if (speed > 2.5f){
+                    speed = speed / 1.5f;
+                }
+                
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
---
+++ Assets/Scripts/RainScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainScript : MonoBehaviour
+{
+    private float rainchance;
+    private ParticleSystem particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        StartCoroutine(rainclock());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator rainclock(){
+        while(true){
+        rainchance = Random.Range(0.0f, 10.0f);
+        if (rainchance <= 6.0f){
+            if(!particle.isPlaying){
+                var main = particle.main;
+                main.duration = Random.Range(0.0f, 30.0f);
+                particle.Play(true);
+            }
+        }
+            yield return new WaitForSeconds(15.0f);
+        }
+    }
+}
